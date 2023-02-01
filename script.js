cc36827d81731e7a02364715557f5e5c2ffc341b
Luke Collins <lukenatcollins@gmail.com>
2023-02-01 22:21:55 +1100

    Reformatted code for easy reading & added keyboard support

--- script.js
+++ script.js
@@ -1,74 +1,26 @@
-var displayValue = document.querySelector('#displayValue');
+//Variables
 var operator = '';
 var firstInput = '';
 var secondInput = '';
+const operatorList = ['+', '-', '*', '/'];
 
-function add(a, b){
-    return a + b;
-}
-
-function subtract(a, b){
-    return a - b;
-}
-
-function multiply(a, b){
-    return a * b;
-}
-
-function divide(a, b){
-    return a / b;
-}
-
-function operate(firstOperand, nextOperand, operator){
-    if(operator == "+"){
-        return add(firstOperand, nextOperand);
-    }
-    if(operator == "-"){
-        return subtract(firstOperand, nextOperand);
-    }
-    if(operator == "*"){
-        return multiply(firstOperand, nextOperand);
-    }
-    if(operator == "/"){
-        return divide(firstOperand, nextOperand);
-    }
-}
-
-function inputKey(display, button){
-    if(display.textContent === '0'){
-        display.textContent = button;
-    } else{
-    display.textContent = display.textContent.concat(button);
-    }
-}
-
+var displayValue = document.querySelector('#displayValue');
 const keypad = document.querySelectorAll("#number");
 const operatorKeys = document.querySelectorAll("#operator");
 const equalKey = document.querySelector("#equals");
 const clearKey = document.querySelector("#clear");
 
+
+//Event Listeners
 keypad.forEach((button) => {
     button.addEventListener('click', function(){
-        inputKey(displayValue, button.textContent);
+        inputKey(button.textContent);
     });
 });
 
-
-const operatorList = ['+', '-', '*', '/'];
-
 operatorKeys.forEach((button) => {
     button.addEventListener('click', function(){
-        if (operatorList.some((element) => element == displayValue.textContent.charAt(displayValue.textContent.length-1))){
-            displayValue.textContent = displayValue.textContent.slice(0, -1).concat(button.textContent);
-        } else if(operator){
-            equalsKey();
-            firstInput = displayValue.textContent;
-            displayValue.textContent = displayValue.textContent.concat(button.textContent);
-        } else if(!operator){
-            firstInput = displayValue.textContent;
-            displayValue.textContent = displayValue.textContent.concat(button.textContent);
-        } 
-        operator = button.textContent;
+        addOperator(button.textContent);
     });
 });
 
@@ -77,11 +29,71 @@ equalKey.addEventListener('click', function(){
 })
 
 clearKey.addEventListener('click', function(){
+    clearAll();
+})
+
+window.addEventListener('keydown', function(e){
+    processInput(e.key);
+});
+
+//Helper Functions
+function processInput(key){
+    if(key.match(/^-?\d+$/)){
+        inputKey(key);
+    }
+    if(operatorList.some((element) => element == key)){
+        addOperator(key);
+    }
+    if(key == '=' || key == 'Enter'){
+        equalsKey();
+    }
+}
+
+function operate(firstOperand, nextOperand, operator){
+    if(operator == "+"){
+        return firstOperand + nextOperand;
+    }
+    if(operator == "-"){
+        return firstOperand - nextOperand;
+    }
+    if(operator == "*"){
+        return firstOperand * nextOperand;
+    }
+    if(operator == "/"){
+        return firstOperand / nextOperand;
+    }
+}
+
+
+//Button Functions
+function inputKey(button){
+    if(displayValue.textContent === '0'){
+        displayValue.textContent = button;
+    } else{
+    displayValue.textContent = displayValue.textContent.concat(button);
+    }
+}
+
+function addOperator(button){
+    if (operatorList.some((element) => element == displayValue.textContent.charAt(displayValue.textContent.length-1))){
+        displayValue.textContent = displayValue.textContent.slice(0, -1).concat(button);
+    } else if(operator){
+        equalsKey();
+        firstInput = displayValue.textContent;
+        displayValue.textContent = displayValue.textContent.concat(button);
+    } else if(!operator){
+        firstInput = displayValue.textContent;
+        displayValue.textContent = displayValue.textContent.concat(button);
+    } 
+    operator = button;
+}
+
+function clearAll(){
     displayValue.textContent = '0';
     firstInput = '';
     secondInput = '';
     operator = '';
-})
+}
 
 function equalsKey(){
     if(!firstInput || !operator || operatorList.some((operator) => operator == displayValue.textContent.charAt(displayValue.textContent.length-1))){
