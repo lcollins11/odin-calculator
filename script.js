2c34fa79e3b162c6d629d20618eef6a22391791b
Luke Collins <lukenatcollins@gmail.com>
2023-02-06 17:30:55 +1100

    Added button & logic to add more rows to the case calculator

--- script.js
+++ script.js
@@ -116,17 +116,25 @@ function equalsKey(){
 }
 
 
-const gridInputs = document.querySelectorAll(".grid-input");
+var caseInputs = document.querySelectorAll(".case-input");
 
-const subTotals = document.querySelectorAll('[data-column="subtotal"]');
+var subTotals = document.querySelectorAll('[data-column="subtotal"]');
 
-gridInputs.forEach(input => {
+caseInputs.forEach(input => {
     input.addEventListener('input', () => {
         updateTotals();
     })
 })
 
 function updateTotals(){
+    updateSubTotals();
+}
+
+var caseTotal = document.querySelector('[data-type="case-total"]');
+var gridSize = subTotals.length;
+
+function updateSubTotals(){
+    var total = 0;
     subTotals.forEach(subTotal => {
         const currentRow = subTotal.dataset.row;
         var price = 0;
@@ -140,5 +148,61 @@ function updateTotals(){
             quantity = rowQuantity.value;
         }
         subTotal.textContent = price * quantity;
+        total += price * quantity;
     })
+    console.log(total);
+    caseTotal.textContent = total;
 }
+
+const caseContainer = document.querySelector('.container-case');
+const addRowButton = document.querySelector('[data-type="case-addRow"]');
+
+addRowButton.addEventListener('click', () =>{
+    addRow();
+})
+
+/**
+                                <div data-row="1" data-column="subtotal" class="case-subtotal">0</div>
+
+ */
+
+function addRow() {
+    //product
+    var newRowProduct = document.createElement('input');
+    newRowProduct.dataset.row = gridSize+1;
+    newRowProduct.dataset.column = "product";
+    newRowProduct.classList.add("case-input");
+    newRowProduct.type = "text";
+    caseContainer.appendChild(newRowProduct);
+    //price
+    var newRowPrice = document.createElement('input');
+    newRowPrice.dataset.row = gridSize+1;
+    newRowPrice.dataset.column = "price";
+    newRowPrice.classList.add("case-input");
+    newRowPrice.type = "text";
+    caseContainer.appendChild(newRowPrice);
+    newRowPrice.addEventListener('input', () => {
+        updateTotals();
+    })
+    //quantity
+    var newRowQuantity = document.createElement('input');
+    newRowQuantity.dataset.row = gridSize+1;
+    newRowQuantity.dataset.column = "quantity";
+    newRowQuantity.classList.add("case-input");
+    newRowQuantity.type = "text";
+    caseContainer.appendChild(newRowQuantity);
+    newRowQuantity.addEventListener('input', () => {
+        updateTotals();
+    })
+    //subtotal
+    var newRowSubtotal = document.createElement('div');
+    newRowSubtotal.dataset.row = gridSize+1;
+    newRowSubtotal.dataset.column = "subtotal";
+    newRowSubtotal.classList.add("case-subtotal");
+    newRowSubtotal.textContent = 0;
+    caseContainer.appendChild(newRowSubtotal);
+    subTotals = document.querySelectorAll('[data-column="subtotal"]');
+    caseInputs = document.querySelectorAll(".case-input");
+    gridSize++;
+}
+
